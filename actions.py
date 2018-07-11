b1f2029e45da8d373a1418ebf71d00975ac1a1d5
Arvind <arvindnatarajan06@gmail.com>
2018-07-12 01:20:49 +0530

    On way to victory

--- actions.py
+++ actions.py
@@ -3,13 +3,13 @@ from model import Mail
 
 gmail_handler = GmailAPI()
 
-message_dict_list = gmail_handler.get_emails()
-
-# Sync mails to DB
-for message_dict in message_dict_list:
-    try:
-        # May throw exception if the mail exists
-        Mail.insert(**message_dict).execute()
-    except Exception as e:
-        print (e)
+def sync_mails_to_db():
+    # Sync mails to DB
+    message_dict_list = gmail_handler.get_emails()
+    for message_dict in message_dict_list:
+        try:
+            # May throw exception if the mail exists
+            Mail.insert(**message_dict).execute()
+        except Exception as e:
+            print (e)
 
