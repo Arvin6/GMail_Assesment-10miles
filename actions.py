646b04f4e7ed78ec56c4c26dce60572d40091f1d
Arvind <arvindnatarajan06@gmail.com>
2018-07-14 00:30:25 +0530

    Rules parsing

--- actions.py
+++ actions.py
@@ -1,5 +1,6 @@
 import json
 import re
+from datetime import datetime, timedelta
 
 import peewee
 from gmail import GmailAPI
@@ -19,7 +20,7 @@ def sync_mails_to_db():
         except Exception as e:
             print (e)
 
-def parse_for_query(condition):
+def get_clause_list(condition_list):
 
     def get_key(key):
         if key == "from":
@@ -32,38 +33,50 @@ def parse_for_query(condition):
             return Mail.message
         return None
     
-    def validate_fields(key, comparison, value):
+    def validate_fields(key=None, comparison=None, value=None):
         valid_key = get_key(key)
         if value and valid_key and comparison:
             return valid_key, comparison, value
-        raise Exception("Invalid format \"{key}:{operator}:{value}\"".format(key=key,value=value,operator=comparison))
+        raise Exception("Invalid format \"{key}:{operator}:{value}\""
+                                    .format(key=key,value=value,operator=comparison))
 
-    operators = condition[1::2]
-    condition_list = condition[0::2]
+    def get_iso_delta(value):
+        number = int(value[:-1])
+        scale = value[-1]
+        if scale == "h":
+            return (datetime.utcnow() - timedelta(hours=number)).isoformat()
+        elif scale == "m":
+            return (datetime.utcnow() - timedelta(minutes=number)).isoformat()
+        elif scale == "d":
+            return (datetime.utcnow() - timedelta(days=number)).isoformat()
+        raise Exception("Invalid scale")
+
     clause_list = []
-    for condition in condition_list:
-        try:
+    try:
+        for condition in condition_list:
             condition_split = condition.split(":")
             key, comparison, value = validate_fields(*condition_split)
             if value:
                 if comparison == "equals":
                     clause_list.append (key == value)
-                elif comparison == "in":
-                    value = json.loads(value)
-                    clause_list.append (key << value)
+                elif comparison == "contains":
+                    clause_list.append (key ** value)
                 elif comparison == "not":
                     clause_list.append (key != value)
 
                 elif comparison == "lt" and condition_split[0] == "date":
-                    clause_list.append (key < value)
-                elif comparison == "gt" and condition_split[0] == "date":
+                    value = get_iso_delta(value)
                     clause_list.append (key > value)
+                elif comparison == "gt" and condition_split[0] == "date":
+                    value = get_iso_delta(value)
+                    clause_list.append (key < value)
                 else:
-                    raise Exception("Invalid format \"{key}:{operator}:{value}\"".format(key=key,value=value,operator=comparison))
-            print (clause_list)
-        except Exception as e:
-            print (e)
-        
+                    raise Exception("Invalid format \"{key}:{operator}:{value}\""
+                                            .format(key=key,value=value,operator=comparison))
+        return clause_list
+    except Exception as e:
+        print (e)
+
 
 if __name__ == "__main__":
     if input("Would you like to sync the mails? (y/n)") == "y":
@@ -75,4 +88,21 @@ if __name__ == "__main__":
         condition = rule.get('condition')
         action = rule.get('action')
         if condition:
-            parse_for_query(condition)
+            operators = condition[1::2]
+            condition_list = condition[0::2]
+        clause_list = get_clause_list(condition_list)
+        expression = clause_list.pop(0)
+        while clause_list:
+            clause = clause_list.pop(0)
+            operator = operators.pop(0)
+            if operator=="AND":
+                expression = (expression & clause)
+            elif operator=="OR":
+                expression = (expression | clause)
+            elif operator=="NOT":
+                expression |= ~(clause)
+        result = Mail.select().where(expression)
+        print (result.sql())
+        for row in result:
+            print (row.gid, row.sender)
+
