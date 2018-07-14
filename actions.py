88d92c17115a5e5587b22446688fdab7ad3c48cb
Arvind <arvindnatarajan06@gmail.com>
2018-07-14 14:35:15 +0530

    Working prototype

--- actions.py
+++ actions.py
@@ -6,8 +6,10 @@ import peewee
 from gmail import GmailAPI
 from model import Mail, db
 
-
-gmail_handler = GmailAPI()
+try:
+    gmail_handler = GmailAPI()
+except Exception as e:
+    print (e)
 
 def sync_mails_to_db():
     # Sync mails to DB
@@ -19,6 +21,24 @@ def sync_mails_to_db():
             pass
         except Exception as e:
             print (e)
+
+def mark_mails_as_read(message_ids):
+    print ("{} will be marked as read".format(message_ids))
+    labels = ['UNREAD']
+    gmail_handler.remove_labels(message_ids=message_ids, labels=labels)
+
+def mark_mails_as_important(message_ids):
+    labels = ['IMPORTANT']
+    gmail_handler.add_labels(message_ids, labels)
+
+def add_labels_to_mail(message_ids, labels):
+    if not isinstance(labels, list):
+        labels = [labels]
+    labels = [label.upper() for label in labels]
+    try:
+        gmail_handler.add_labels(message_ids, labels)
+    except Exception as e:
+        print (e)
 
 def get_clause_list(condition_list):
 
@@ -84,25 +104,45 @@ if __name__ == "__main__":
 
     with open('rules.json', 'r') as rule_fp:
         rules = json.load(rule_fp)
+    
     for rule in rules:
         condition = rule.get('condition')
         action = rule.get('action')
+
         if condition:
             operators = condition[1::2]
             condition_list = condition[0::2]
+        
         clause_list = get_clause_list(condition_list)
         expression = clause_list.pop(0)
+        
         while clause_list:
             clause = clause_list.pop(0)
             operator = operators.pop(0)
+        
             if operator=="AND":
                 expression = (expression & clause)
+        
             elif operator=="OR":
                 expression = (expression | clause)
+        
             elif operator=="NOT":
                 expression |= ~(clause)
-        result = Mail.select().where(expression)
-        print (result.sql())
-        for row in result:
-            print (row.gid, row.sender)
+        
+        result = Mail.select(Mail.gid).where(expression)
+        # print (result.sql())
+        message_ids = [row.gid for row in result]
+        if message_ids:
+            if action.get('mark_as_read'):
+                mark_mails_as_read(message_ids)
+            
+            if action.get('mark_as_important'):
+                mark_mails_as_important(message_ids)
+            
+            if action.get('mark_as_spam'):
+                mark_mails_as_spam(message_ids)
 
+            labels = action.get('add_labels')
+            if labels:
+                gmail_handler.get_labels()
+                add_labels_to_mail(message_ids, labels)
--- gmail.py
+++ gmail.py
@@ -1,4 +1,5 @@
 from apiclient.discovery import build
+from apiclient import errors
 from httplib2 import Http
 from oauth2client import file, client, tools
 import dateutil.parser as parser
@@ -10,7 +11,9 @@ userId = "me"
 
 class GmailAPI:
     def __init__(self):
+        self.labels = {}
         self.get_credentials()
+        self.get_labels()
 
     def get_credentials(self):
         creds = store.get()
@@ -22,15 +25,80 @@ class GmailAPI:
     def get_message_id_list(self, query=None, labels=None):
         message_id_list = []
         result = (self.service.users().messages()
-                .list(userId=userId, labelIds=labels, q=query).execute())
+                    .list(userId=userId, labelIds=labels, q=query).execute())
         message_id_list.extend(result['messages'])
-
+        # Commenting this since syncing 1000's of mail at a time will take more time
         # while 'nextPageToken' in result:
         #     result = (self.service.users().messages()
         #             .list(userId=userId, labelIds=labels, q=query, pageToken=result['nextPageToken']).execute())
         #     message_id_list.extend(result['messages'])                  
         message_id_list = map(lambda x: x['id'], message_id_list)
         return message_id_list
+
+    def get_labels(self):
+        results = self.service.users().labels().list(userId='me').execute()
+        labels = results.get('labels', [])
+        for label in labels:            
+            self.labels[label['name']] = label['id']
+
+    def create_label(self, name):
+        label_color = {
+            "textColor": "#ffffff",
+            "backgroundColor": "#44b984"
+        }
+        label_dict = {
+            "name": name.upper(),
+            "messageListVisibility": "show",
+            "labelListVisibility": "labelShow",
+            "color": label_color
+        }
+        try:
+            label = self.service.users().labels().create(
+                userId=userId,
+                body=label_dict
+            ).execute()
+            return label
+        except errors.HttpError as e:
+            print (e)
+            return False
+
+    def add_labels(self, message_ids, labels):
+        label_id_list = []
+        for label in labels:
+            label_id = self.labels.get(label)
+            if not label_id:
+                created_label = self.create_label(label)
+                if created_label:
+                    print ("Created a new label {}".format(created_label['name']))
+                    label_id = created_label['id']
+                else:
+                    raise Exception("Unable to create label - {}".format(label))
+            label_id_list.append(label_id)
+
+        batch = self.service.users().messages().batchModify(
+            userId=userId,
+            body={
+                "ids":message_ids,
+                "addLabelIds": label_id_list,
+                "removeLabelIds": []
+            }
+        )
+        result = batch.execute()
+        print (result)
+        print ("{} mails added to the {} label".format(len(message_ids), labels))
+    
+    def remove_labels(self, message_ids, labels):
+        batch = self.service.users().messages().batchModify(
+            userId=userId,
+            body={
+                "ids":message_ids,
+                "addLabelIds": [],
+                "removeLabelIds": labels
+            }
+        )
+        result = batch.execute()
+        print (result)
+        
 
     def get_message_dict_list(self, message_id_list):
         batch_request = self.service.new_batch_http_request()
@@ -57,7 +125,9 @@ class GmailAPI:
             mail_dicts.append(message_dict)
         
         for m_id in message_id_list:
-            batch_request.add(self.service.users().messages().get(userId=userId, id=m_id, format="metadata", metadataHeaders=["From", "Subject", "Date"]),
+            batch_request.add(self.service.users().messages()
+                            .get(userId=userId, id=m_id, format="metadata",
+                                 metadataHeaders=["From", "Subject", "Date"]),
                 append_mail_dicts
             )
         batch_request.execute()
